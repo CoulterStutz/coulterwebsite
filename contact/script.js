fe9eb26edaa7ed8fa9b7688ccc3336309cd6fc3c
Coulter <coulterstutz@gmail.com>
2024-02-27 12:02:43 -0700

    [CONTACT]: RESTART CONTACT PAGE

--- contact/script.js
+++ contact/script.js
@@ -1,10 +1,3 @@
-var ticket_type = document.getElementById("ticket_type");
-
-var job_type = document.getElementById("job_type")
-var job_type_text = document.getElementById("job_type_text")
-var web_type = document.getElementById("web_type")
-var web_type_text = document.getElementById("web_type_text")
-
 let bitcoin = document.getElementById("bitcoin")
 let btc_qr = document.getElementById("btc_qr")
 let btc_enabled = false
@@ -35,39 +28,9 @@ let etc_enabled = false
 
 let t_title = document.getElementById("t_title")
 
-let t_type = "";
-let j_type = "";
-
-ticket_type.onchange = function(){
-    if (ticket_type.value == "job_related"){
-        job_type.style.display = "block";
-        job_type_text.style.display = "block";
-        t_type = "job_related";
-    } else {
-        job_type.style.display = "none";
-        job_type_text.style.display = "none";
-        t_type = "bug";
-    }
-}
-
-job_type.onchange = function(){
-    if (job_type.value == "website") {
-        web_type.style.display = "block";
-        web_type_text.style.display = "block";
-        j_type = "website";
-    } else {
-        web_type.style.display = "none";
-        web_type_text.style.display = "none";
-    }
-}
-
 
 
 window.onload = function(){
-    job_type.style.display = "none";
-    job_type_text.style.display = "none";
-    web_type.style.display = "none";
-    web_type_text.style.display = "none";
     btc_qr.style.display = "none";
     eth_qr.style.display = "none";
     sol_qr.style.display = "none";
